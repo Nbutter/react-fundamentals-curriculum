a8a4f68ab7bb75fff336213f226aa659a9d69840
Nicholas Butterworth <nicholas.butterworth@gmail.com>
2018-07-31 18:23:38 -0400

    hello world part 2

--- app/Index.js
+++ app/Index.js
@@ -1,6 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-//require('./index.css');
-//const Hello = require('./components/Hello');
+const React = require('react');
+const ReactDOM = require('react-dom');
+require('./index.css');
+const Hello = require('./components/Hello');
 
-ReactDOM.render(<h1>Hello, World!</h1>, document.getElementById('hello'));
+ReactDOM.render(<Hello />, document.getElementById('hello'));
--- app/components/Hello.js
+++ app/components/Hello.js
@@ -5,7 +5,7 @@ class Hello extends React.Component {
   render() {
     return (
       <div className='container'>
-        <h1>Hello!<h1>
+        <h1>Hello, world!</h1>
       </div>
     )
   }
